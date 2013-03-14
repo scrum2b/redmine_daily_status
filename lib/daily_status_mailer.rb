352beb0f407665a1c0cdec04d091fcc0e4907bee
Amol Pujari <amolpujari@gmail.com>
2013-03-15 01:23:20 +0530

    trial 2.3.5

--- lib/daily_status_mailer.rb
+++ lib/daily_status_mailer.rb
@@ -5,7 +5,8 @@ class DailyStatusMailer < Mailer
   unloadable
   ActionMailer::Base.prepend_view_path(File.join(File.dirname(__FILE__), '../app/views'))
 
-  include Redmine::I18n	
+  include Redmine::I18n
+  default :content_type => "text/html"
 
   def send_daily_status(daily_status)
     @recipients = daily_status.project.members.collect {|m| m.user}.collect {|u| u.mail}
