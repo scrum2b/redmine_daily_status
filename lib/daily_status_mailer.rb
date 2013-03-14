06db4d516e1c723836da55db0e3573a2e852e4a1
Amol Pujari <amolpujari@gmail.com>
2013-03-15 02:55:03 +0530

    done

--- lib/daily_status_mailer.rb
+++ lib/daily_status_mailer.rb
@@ -6,13 +6,12 @@ class DailyStatusMailer < Mailer
   ActionMailer::Base.prepend_view_path(File.join(File.dirname(__FILE__), '../app/views'))
 
   include Redmine::I18n
-  default :content_type => "text/html"
 
   def send_daily_status(daily_status)
     @recipients = daily_status.project.members.collect {|m| m.user}.collect {|u| u.mail}
     @project_name = daily_status.project.name
     @daily_status_content = daily_status.content
     @login_user_name = User.current.name;
-    mail(:to => @recipients, :subject => l(:label_email_subject).to_s+daily_status.project.name)
+    mail(:to => @recipients, :subject => l(:label_email_subject).to_s+daily_status.project.name, :content_type => "text/html")
   end
 end
