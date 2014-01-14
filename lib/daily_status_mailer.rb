f0730c21026b0057d3f3177297a76bde7466e3b9
Vishal Mene <vishal@lark.(none)>
2014-01-14 15:44:47 +0530

    E-Mail Header Issue.Removed content type attribute from mail function.

--- lib/daily_status_mailer.rb
+++ lib/daily_status_mailer.rb
@@ -8,10 +8,9 @@ class DailyStatusMailer < Mailer
   include Redmine::I18n
 
   def send_daily_status( daily_status, recipients)
-  	print recipients
     @project_name = daily_status.project.name
     @daily_status_content = daily_status.content
     @login_user_name = User.current.name;
-    mail(:to => recipients, :subject => "#{l(:label_email_subject)} #{@project_name}", :content_type => "text/html")
+    mail(:to => recipients, :subject => "#{l(:label_email_subject)} #{@project_name}")
   end
 end
